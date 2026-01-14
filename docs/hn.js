0e0351e71b10e576eeeee57a69bb94f9b120d982
Seiichi <ar.seiichi.1029@gmail.com>
2026-01-14 22:54:19 +0900

    feat: add hackernews top

---
+++ docs/hn.js
@@ -0,0 +1,6 @@
+﻿fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+  .then(r=>r.json())
+  .then(ids=>Promise.all(ids.slice(0,20).map(id=>
+    fetch("https://hacker-news.firebaseio.com/v0/item/"+id+".json").then(r=>r.json())
+  )))
+  .then(items=>console.log(items));
